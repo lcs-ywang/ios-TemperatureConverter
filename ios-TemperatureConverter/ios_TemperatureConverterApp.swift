600c26c393e12a4738e80fe09892e87e42c381bc
yining wang <ywang@lcs.on.ca>
2021-02-15 20:49:31 -0500

    Initial Commit

---
+++ ios-TemperatureConverter/ios_TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_TemperatureConverterApp.swift
+//  ios-TemperatureConverter
+//
+//  Created by Yining Wang on 2021-02-15.
+//
+
+import SwiftUI
+
+@main
+struct ios_TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
