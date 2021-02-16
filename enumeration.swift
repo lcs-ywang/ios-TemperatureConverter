c8339c6108dbd8858c6fd4a6f45bfb3807f9531b
yining wang <ywang@lcs.on.ca>
2021-02-15 20:51:44 -0500

    Enumeration and remotes got set up

---
+++ enumeration.swift
@@ -0,0 +1,14 @@
+//
+//  enumeration.swift
+//  ios-TemperatureConverter
+//
+//  Created by Yining Wang on 2021-02-15.
+//
+
+import Foundation
+
+enum TemperatureUnit: String {
+    case fahrenheit = "fahrenheit"
+    case kelvin = "kelvin"
+    case celcius = "celcius"
+}
